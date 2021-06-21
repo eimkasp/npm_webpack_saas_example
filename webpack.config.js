c419f62ab636c7ab30cb704c658fe4a8db0f527d
Eimantas <eim.kasp@gmail.com>
2021-06-21 20:05:12 +0300

    updates for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const devMode = process.env.NODE_ENV !== 'production';
+
 module.exports = {
+  plugins: [new MiniCssExtractPlugin()],
   module: {
     rules: [
       {
         test: /\.s[ac]ss$/i,
         use: [
+           MiniCssExtractPlugin.loader,
           // Creates `style` nodes from JS strings
-          "style-loader",
           // Translates CSS into CommonJS
           "css-loader",
           // Compiles Sass to CSS
